089dbd7b7bed13a387c211b1c40736787c61ab88
Rob Lopez <rob.lopez@gmail.com>
2021-09-21 23:05:30 -0700

    aws setup test

--- aws/setup.sh
+++ aws/setup.sh
@@ -1,3 +1,4 @@
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get install -y nodejs
-git clone 
+sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3100
+
